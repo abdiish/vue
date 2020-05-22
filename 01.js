47c6a3659740fa169e4b2fe46eabbd930b5935d9
abdiish <ing.ti.upmh@gmail.com>
2020-05-21 20:56:41 -0500

    Instanciar vue, interpolacion, uso de for para recorrer arreglo de objetos y string

---
+++ 01.js
@@ -0,0 +1,13 @@
+//Instanciar vue, llamada a libreria 
+const app = new Vue({
+    el: '#app',
+    data: {
+        titulo: 'Curso vue.js', 
+        frutas: ['Manzana','Platano','Piña','Mango','Cereza'],
+        personas: [
+            {nombre:'Juan', edad:20},
+            {nombre:'Luis', edad:21},
+            {nombre:'Cesar,', edad:23}
+        ] 
+    }
+})
